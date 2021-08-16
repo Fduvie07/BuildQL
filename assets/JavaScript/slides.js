2638af375f9873799b713e40a6e7854b059f7a3f
Favour Eduviere <fduvie@gmail.com>
2021-08-16 18:52:19 +0100

    Done the auto slide

--- assets/JavaScript/slides.js
+++ assets/JavaScript/slides.js
@@ -3,7 +3,7 @@ showSlides();
 
 function showSlides() {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
+    var slides = document.getElementsByClassName("learn-slides");
     var dots = document.getElementsByClassName("dot");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -17,5 +17,32 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); //Change image every 2 seconds
+    setTimeout(showSlides, 5000); //Change image every 2 seconds
 }
+
+/*
+function changeToPs() {
+    document.getElementById("slideImage").src="assets/img/photoshop_screen.png";
+}
+
+function changeToAi() {
+    document.getElementById("slideImage").src="assets/img/Ai-screenshot.png";
+}
+function changeToWhim() {
+    document.getElementById("slideImage").src="assets/img/Whimsical-screenshot.png";
+}
+function changeToFigma() {
+    document.getElementById("slideImage").src="assets/img/figma_screen.png";
+}
+function changeToFramerX() {
+    document.getElementById("slideImage").src="assets/img/framer_screen.png";
+}
+function changeToJs() {
+    document.getElementById("slideImage").src="assets/img/js_screen.png";
+}
+function changeToHtml() {
+    document.getElementById("slideImage").src="assets/img/HTML-screenshot.png";
+}
+function changeToCss() {
+    document.getElementById("slideImage").src="assets/img/Css-screenshot.png";
+}*/
